1a37fe8ea33487cc75f1483e56e2cc8575979b39
nikhil <nk0158390@gmail.com>
2024-02-09 12:30:44 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Task_Project.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3 style="text-align: center; background-color: #C0C0C0; color: #0000FF; font-weight: bold; height: 33px;">Testing process</h3>
+        </div>
+        <div style="text-align: center">
+          <b>Country :</b>  <asp:DropDownList ID="DropDownList1" runat="server" Width="243px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True"></asp:DropDownList>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>State :</b>&nbsp;
+            <asp:DropDownList ID="DropDownList2" runat="server" Width="233px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+            </asp:DropDownList>
+            <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; City :</b>&nbsp;<asp:DropDownList ID="DropDownList3" runat="server" Width="233px">
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Task_Project
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Employee_DB;Integrated Security=True"); // nik...
+        SqlCommand cmd = new SqlCommand();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindCountryList();
+            }
+        }
+        private void BindCountryList()
+        {
+            try
+            {
+                        cmd.CommandText = "SELECT CountryId, County FROM Country";
+                        cmd.Connection = con;
+                        con.Open();
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        DropDownList1.DataSource = dt;
+                        DropDownList1.DataValueField = "CountryId";
+                        DropDownList1.DataTextField = "County";
+                        DropDownList1.DataBind();
+                        con.Close();
+                        DropDownList1.Items.Insert(0, new ListItem("Please select country", "0"));
+                        DropDownList2.Items.Insert(0, new ListItem("Please select state", "0"));
+                        DropDownList3.Items.Insert(0, new ListItem("Please select City", "0"));
+                    
+            }
+            catch { }
+        }
+       
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)    // DropDownList Country .........
+        {
+            try
+            {
+                cmd.CommandText = "SELECT StateID,State FROM countryState WHERE CountryId = @CountryId";
+                cmd.Parameters.AddWithValue("@CountryId", DropDownList1.SelectedValue);
+                cmd.Connection = con;
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                DropDownList2.DataSource = dt;
+                DropDownList2.DataValueField = "StateID";
+                DropDownList2.DataTextField = "State";
+                DropDownList2.DataBind();
+                con.Close();
+                DropDownList2.Items.Insert(0, new ListItem("Please select state", "0"));
+            }
+            catch { }
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)    // DropDownList State .........
+        {
+            try
+            {
+                cmd.CommandText = "SELECT CountryId,City FROM State_City WHERE CountryId = '"+ DropDownList2.SelectedValue + "'";
+                cmd.Connection = con;
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                DropDownList3.DataSource = dt;
+                DropDownList3.DataValueField = "CountryId";
+                DropDownList3.DataTextField = "City";
+                DropDownList3.DataBind();
+                con.Close();
+
+                DropDownList3.Items.Insert(0, new ListItem("Please select city", "0"));
+            }
+            catch { }
+        }
+    }
+}
